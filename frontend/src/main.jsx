ff798c64661d1dc79e1a8c8de870c71883cd8bbf
Kalathuru Varshitha <varshithakalathuru@gmail.com>
2026-09-05 13:24:09 +0530

    Set basename for BrowserRouter in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/sih">
       <AuthProvider>
         <App />
       </AuthProvider>
